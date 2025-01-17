ee2fb445584aa69ccc2da15b1258e63c7a5b319a
Danya Sherbini <danyasherbini9@gmail.com>
2025-01-17 11:06:34 -0600

    Update config.py

--- app/db/config.py
+++ app/db/config.py
@@ -32,24 +32,38 @@ Output: string values
 #    return values
 
 
-from configparser import ConfigParser
 import os
+from configparser import ConfigParser
 
-def config(section='postgres'):
-    if os.getenv('DIGITALOCEAN_ENV', False):  # Check if running in DigitalOcean; if so, load credentials from digitial ocean environmental variables
-        return {
-            'host': os.getenv('DB_HOST'),
-            'port': os.getenv('DB_PORT'),
-            'database': os.getenv('DB_NAME'),
-            'user': os.getenv('DB_USER'),
-            'password': os.getenv('DB_PASSWORD'),
-            'sslmode': os.getenv('SSL_MODE')
-        }
-    else:  # Load from credentials.ini for local development
-        parser = ConfigParser()
+def config(section):
+    # First, try to get configuration from Digitial Ocean environment variables
+    db_config = {
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT'),
+        'database': os.getenv('DB_NAME'),
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'sslmode': os.getenv('SSL_MODE')
+    }
+
+    # Check if environment variables are empty
+    if all(value is None for value in db_config.values()):
+        # If environment variables are not set, fall back to the credentials.ini file
         filename = 'db/credentials.ini'
-        parser.read(filename)
-        if parser.has_section(section):
-            return {key: val for key, val in parser.items(section)}
+        parser = ConfigParser()
+        if parser.read(filename):
+            if parser.has_section(section):
+                db_config = {
+                    'host': parser.get(section, 'host'),
+                    'port': parser.get(section, 'port'),
+                    'dbname': parser.get(section, 'database'),
+                    'user': parser.get(section, 'user'),
+                    'password': parser.get(section, 'password'),
+                    'sslmode': parser.get(section, 'sslmode')
+                }
+            else:
+                raise Exception(f"Section {section} not found in the {filename} file")
         else:
-            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+            raise Exception(f"Could not read {filename} file")
+
+    return db_config
