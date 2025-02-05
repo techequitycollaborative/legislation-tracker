9d66b43a84dda13ae58f5997770b5084827c3773
Danya Sherbini <danyasherbini9@gmail.com>
2025-02-04 19:29:38 -0600

    added simple version of google auth + made dashboard storage persisent across user sessions

--- app/dashboard.py
+++ app/dashboard.py
@@ -8,30 +8,63 @@ Created on Dec 2, 2024
 Page to add bills to user's private dashboard
 """
 
-
 import streamlit as st
 import pandas as pd
 from utils.aggrid_styler import draw_bill_grid
+from utils.utils import format_bill_history, get_bill_topics, keywords, to_csv
+from db.query import get_my_dashboard_bills
+from utils.dashboard_utils import display_dashboard_details
 
 
 # Page title
 st.title('Dashboard')
 
+# Get user email
+user_email = st.session_state["user_info"].get("email")
+
 # Clear dashboard button
-col1, col2 = st.columns([3, 1])
+col1, col2 = st.columns([4, 1])
 with col2:
-    if st.button('Clear Dashboard'):
+    if st.button('Clear Dashboard', use_container_width=True, type='primary'):
         st.session_state.selected_bills = []  # Clear session state
         st.success('Dashboard cleared!')
 
-# Display selected bills in the dashboard
-if 'selected_bills' in st.session_state:
-    st.write('Selected Bills:')
-    dashboard_df = pd.DataFrame(st.session_state.selected_bills)
-    draw_bill_grid(dashboard_df)
+# Fetch the user's saved bills from the database
+db_bills = get_my_dashboard_bills(user_email)
+
+# Minor data processing to match bills table
+db_bills['date_introduced'] = pd.to_datetime(db_bills['date_introduced']).dt.strftime('%Y-%m-%d') # Remove timestampe from date introduced
+db_bills = get_bill_topics(db_bills, keyword_dict= keywords)  # Get bill topics
+db_bills['bill_history'] = db_bills['bill_history'].apply(format_bill_history) #Format bill history
+
+if not db_bills.empty:
+    st.write('Your saved bills:')
+    data = draw_bill_grid(db_bills)
+
+    # Display bill details for dashboard bills
+    if 'selected_bills' not in st.session_state:
+        st.session_state.selected_bills = []
+        
+    selected_rows = data.selected_rows
+
+    if selected_rows is not None and len(selected_rows) != 0:
+            display_dashboard_details(selected_rows)
+
+    # Buttom to download selected bills
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        st.markdown("")
+
+    with col2:
+        st.download_button(
+                label='Download Data as CSV',
+                data=to_csv(db_bills),
+                file_name='my_bills.csv',
+                mime='text/csv',
+                use_container_width=True
+            )
+
 else:
     st.write('No bills selected yet.')
-    
-
 
 
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -10,28 +10,89 @@ Streamlit, an open-source framework to build data apps in Python.
 """
 
 import streamlit as st
+from streamlit_google_auth import Authenticate
 
-# Pages
-welcome = st.Page('welcome.py',title='Welcome',icon='📜')
-bills_tabs_expander = st.Page('bills_tabs.py', title='Bills - Tabs + Expander', icon='📝')
-bills_tabs_dialog = st.Page('bills_tabs_dialog.py', title='Bills - Tabs + Dialog', icon='📝')
-bills_multi = st.Page('bills_multiselect.py', title='Bills - Multiselect + Text', icon='📝')
-bills_topic = st.Page('bills_topic.py', title='Bills', icon='📝')
-legislators = st.Page('legislators.py', title='Legislators', icon='💼')
-calendar = st.Page('calendar2.py', title='Calendar', icon='📅')
-dashboard = st.Page('dashboard.py', title='My Dashboard', icon='📌')
-
-# Build navigation bar
-pg = st.navigation([welcome,bills_topic,legislators,calendar, dashboard])
+# Set page config
 st.set_page_config(page_title='Legislation Tracker', layout='wide')
 
 # Add logo
 logo = './assets/logo.png'
-
 st.logo(
-    logo,
-    link="https://techequity.us"
+        logo,
+        link="https://techequity.us")
+
+# Google Authenticator Setup -- cookies are not being stored properly; need to fix this
+authenticator = Authenticate(
+    secret_credentials_path='google_credentials.json',
+    cookie_name='my_cookie_name',
+    cookie_key='this_is_secret',
+    redirect_uri='http://localhost:8501',  # Change this for production
+    cookie_expiry_days=30,
 )
 
-# Run the pages
-pg.run()
+# Authenticate user
+authenticator.check_authentification()
+
+# If the user is not authenticated, show login page
+if not st.session_state.get('connected', False):
+    # Set the login page title + center it
+    st.markdown(
+    """
+    <h3 style="text-align: center;">Login to the CA Legislation Tracker</h3>
+    """, 
+    unsafe_allow_html=True)
+    # Show the login button and handle the login
+    authenticator.login()
+
+else:
+    # User is authenticated, show the main content
+    user_info = st.session_state['user_info']
+    user_email = user_info.get('email')  # This will be used as the unique user identifier
+
+
+    # Add page navigation for the authenticated user
+    # Pages
+    welcome = st.Page('welcome.py', title='Welcome', icon='📜')
+    bills_tabs_expander = st.Page('bills_tabs.py', title='Bills - Tabs + Expander', icon='📝')
+    bills_tabs_dialog = st.Page('bills_tabs_dialog.py', title='Bills - Tabs + Dialog', icon='📝')
+    bills_multi = st.Page('bills_multiselect.py', title='Bills - Multiselect + Text', icon='📝')
+    bills_topic = st.Page('bills_topic.py', title='Bills', icon='📝')
+    legislators = st.Page('legislators.py', title='Legislators', icon='💼')
+    calendar = st.Page('calendar2.py', title='Calendar', icon='📅')
+    dashboard = st.Page('dashboard.py', title='My Dashboard', icon='📌')
+
+    # Build navigation bar
+    pg = st.navigation([welcome, bills_topic, legislators, calendar, dashboard])
+
+    # Run the pages
+    pg.run()
+
+    # Add the logout button to the bottom of the navigation bar
+    # Adding spaces to push the button to the bottom (this is a hack, could be improved)
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.markdown('')
+    st.sidebar.button('Log out', key='logout', on_click=authenticator.logout)
+
+
