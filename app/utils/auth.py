e9ef28e3f15e8d9f59bfaec8a1255e0df20ee3db
Danya Sherbini <danyasherbini9@gmail.com>
2025-02-18 16:36:16 -0600

    Update auth.py

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,33 +1,3 @@
-import paramiko
-import os
-import io
-
-#def fetch_google_credentials_from_droplet():
-    # Load private key from environment variable
-#    private_key = os.getenv("PRIVATE_SSH_KEY")
-
-    # Convert the string to a file-like object
-#    private_key_file = io.StringIO(private_key)  # Use StringIO for string input
-
-    # Initialize the SSH client and load the private key
-#    key = paramiko.RSAKey.from_private_key(private_key_file)
-
-    # Set up SSH connection to the droplet
-#    ssh = paramiko.SSHClient()
-#    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    # Connect to droplet on port 6022
-#    ssh.connect('143.198.149.149', port=6022, username='root', pkey=key)
-
-    # Use SFTP to fetch the google_credentials.json from droplet
-#    sftp = ssh.open_sftp()
-#    local_path = './google_credentials.json'  # Save file locally
-#    sftp.get('/root/auth/google_credentials.json', local_path)
-#    sftp.close()
-#    ssh.close()
-#    return local_path
-
-
 import os
 import io
 import paramiko
