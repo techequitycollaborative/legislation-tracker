861b831cd61a86bce91d12d74a22db7d19fd95e8
Danya Sherbini <danyasherbini9@gmail.com>
2025-12-18 15:09:10 -0600

    sql table creation script updates
    Updated table creation sql scripts to reflect new db architecture (new schema names)

--- app/db/sql/tables/working_group_tables.sql
+++ app/db/sql/tables/working_group_tables.sql
@@ -3,7 +3,7 @@
 -- Create table for working group discussion board comments
 
 -- Table: working_group_dashboard
-CREATE TABLE public.working_group_dashboard (
+CREATE TABLE app.working_group_dashboard (
     working_group_dashboard_id SERIAL PRIMARY KEY,
     added_by_org TEXT,       
     added_by_user TEXT,
@@ -13,12 +13,12 @@ CREATE TABLE public.working_group_dashboard (
 );
 
 -- Table: working_group_discussions
-CREATE TABLE public.working_group_discussions (
+CREATE TABLE app.working_group_discussions (
     id SERIAL PRIMARY KEY,
     bill_number TEXT NOT NULL,
     user_email TEXT NOT NULL,
     comment TEXT NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    reply_to INTEGER REFERENCES public.working_group_discussions(id) ON DELETE CASCADE
+    reply_to INTEGER REFERENCES app.working_group_discussions(id) ON DELETE CASCADE
 );
 
