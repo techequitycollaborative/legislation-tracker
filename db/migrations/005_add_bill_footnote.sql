7242d043af22fe680b5067c20e418e81c25c8b2f
Jessica Wang <jessiwang2000@gmail.com>
2026-04-20 18:34:10 -0700

    SQL syntax fix

--- db/migrations/005_add_bill_footnote.sql
+++ db/migrations/005_add_bill_footnote.sql
@@ -7,7 +7,7 @@ BEGIN;
 
 -- New column for hearing_bills to record special footnotes
 ALTER TABLE snapshot.hearing_bills
-    ADD COLUMN footnote TEXT DEFAULT NULL;
+    ADD COLUMN footnote TEXT DEFAULT NULL,
     ADD COLUMN footnote_symbol CHAR(1) DEFAULT NULL;
 
 COMMIT;
